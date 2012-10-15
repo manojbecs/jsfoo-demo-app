0be5813d4619e7e786e3c42e234999ae18338aa2
Manoj N <manojkumar.n@multunus.com>
2012-10-15 11:59:42 +0530

    First commit

---
+++ support/fixtures.js
@@ -0,0 +1,10 @@
+function loadFormFixture() {
+	/*:DOC += <!DOCTYPE html>
+	<form id='signup-form' action="/" method="post" class="form-horizontal">
+	    <input id="name" name='name' type='text'></input>
+        <input id="email" name='email' type='text'></input>
+        <input id="password" name='password' type='password'></input>    
+        <button class='btn btn-primary' type='submit'>Sign Up</button>
+   	</form>
+   	*/
+}
---
+++ test/signUp.spec.js
@@ -0,0 +1,19 @@
+describe("Sign Up form", function(){
+	beforeEach(function(){
+		/*:DOC += <!DOCTYPE html>
+		<form id='signup-form' action="/" method="post" class="form-horizontal">
+	    	<input id="name" name='name' type='text'></input>
+        	<input id="email" name='email' type='text'></input>
+        	<input id="password" name='password' type='password'></input>    
+        	<button class='btn btn-primary' type='submit'>Sign Up</button>
+   		</form>
+   		*/
+	});
+
+	afterEach(function(){
+	});
+
+	it("should validate presence of name field", function(){
+		
+	});
+});
